540752e71015673c2e157ad357da90be53b7de5e
Evans <evans.ogonda@student.moringaschool.com>
2022-09-01 09:55:05 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.getElementById('main');
+main.remove();
+console.log(main);
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Joash is the champion";
